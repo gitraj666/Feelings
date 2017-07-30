9b64e176e9903eced56debb6faa145baad9a90a6
ramraj666 <pmaurya196@gmail.com>
2017-07-30 11:42:51 +0530

    Now User can add Feelings

---
+++ thoughts/urls.py
@@ -0,0 +1,9 @@
+__author__ = 'PRITHVIRAJ'
+
+from django.conf.urls import url
+from . import views
+
+urlpatterns =[
+    url(r'^create/$',views.CreateThought.as_view(),name="create"),
+
+]
--- thoughts/views.py
+++ thoughts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView
+from .forms import ThoughtForm
+from django.core.urlresolvers import reverse_lazy
 
 # Create your views here.
+
+class CreateThought(LoginRequiredMixin, CreateView):
+    form_class = ThoughtForm
+    success_url = reverse_lazy('users:dashboard')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
