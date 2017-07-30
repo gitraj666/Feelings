def81bd9a9982ffa2468fe37644373faf8ca2968
ramraj666 <pmaurya196@gmail.com>
2017-07-30 18:25:37 +0530

    Now User can add Feelings

--- thoughts/views.py
+++ thoughts/views.py
@@ -9,6 +9,10 @@ class CreateThought(LoginRequiredMixin, CreateView):
     form_class = ThoughtForm
     success_url = reverse_lazy('users:dashboard')
 
+    def get_object(self, queryset=None):
+        select_related =['thoughts']
+        return self.request.user
+
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
